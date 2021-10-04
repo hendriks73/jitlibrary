b1b097d138c4ba59f5167c8e12b473c6df8f7ab3
Hendrik Schreiber <hs@tagtraum.com>
2021-10-04 14:52:39 +0200

    Added more zero object checks.

--- jitlibrary/src/test/java/com/tagtraum/jitlibrary/TestITLibAlbum.java
+++ jitlibrary/src/test/java/com/tagtraum/jitlibrary/TestITLibAlbum.java
@@ -6,7 +6,10 @@
  */
 package com.tagtraum.jitlibrary;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * TestITLibAlbum.
@@ -15,9 +18,27 @@ import org.junit.jupiter.api.Test;
  */
 public class TestITLibAlbum {
 
+    @BeforeAll
+    public static void before() {
+        ITNativeLibraryLoader.loadLibrary();
+    }
+
     @Test
     public void testDummy() {
         final ITLibAlbum nullAlbum = new ITLibAlbum(0L);
+        assertNull(nullAlbum.getName());
+        assertNull(nullAlbum.getTitle());
+        assertNull(nullAlbum.getAlbumArtist());
+        assertNull(nullAlbum.getSortAlbumArtist());
+        assertNull(nullAlbum.getSortTitle());
+        assertSame(0, nullAlbum.getDiscCount());
+        assertSame(0, nullAlbum.getTrackCount());
+        assertSame(0, nullAlbum.getDiscNumber());
+        assertSame(0, nullAlbum.getRating());
+        assertSame(0L, nullAlbum.getId());
+        assertFalse(nullAlbum.isCompilation());
+        assertFalse(nullAlbum.isGapless());
+        assertFalse(nullAlbum.isRatingComputed());
     }
 
 }
